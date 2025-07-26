e0fcc5572e2aa66b280894ca4295e5d2dc215108
Navied_dipu <navieddipu08@gmail.com>
2025-07-26 19:48:34 +0600

    add intrceptor for user and redirect to login

--- src/hooks/UseAxiosScure.jsx
+++ src/hooks/UseAxiosScure.jsx
@@ -1,9 +1,32 @@
-import axios from 'axios'
-import React from 'react'
-const axiosInstance=axios.create({
-    baseURL:'http://localhost:5000',
-    withCredentials:true,
-})
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
 export default function UseAxiosScure() {
- return axiosInstance
+    const {signOutUser}=useAuth()
+    const naviget=useNavigate()
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(response => {
+      return response},
+     error =>{
+        console.log('error intrceptor ', error)
+        if(error.status === 401 || error.status === 403){
+            console.log('need to logout user')
+            signOutUser()
+            .then(()=>{
+                console.log('logout user')
+                naviget('/signin')
+            })
+            .catch(error => console.log(error.massege))
+            
+        }
+
+        return Promise.reject(error)
+    });
+  }, []);
+  return axiosInstance;
 }
